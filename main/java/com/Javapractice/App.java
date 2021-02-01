c54d14a42b8bc796ba4fd1f2e9d071705b555c16
NishaRaut <nisharaut84@gmail.com>
2021-02-01 13:32:51 +0530

    Added: some changes

--- main/java/com/Javapractice/App.java
+++ main/java/com/Javapractice/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello test 1!" );
+        System.out.println( "Hello test 1  Hello test 1!" );
+ 
     }
 }
